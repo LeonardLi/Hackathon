e5a5cd7ca7cb255563bc831d4afd4dbd915cd7e8
nobody <370170920@qq.com>
2015-11-13 13:08:44 +0800

    redis：添加food 库存表和toke&user双射表

--- src/main/java/com/cloudteam/utils/FoodsTableRedis.java
+++ src/main/java/com/cloudteam/utils/FoodsTableRedis.java
@@ -3,31 +3,27 @@ package com.cloudteam.utils;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import com.cloudteam.utils.DBHelper;
-
-public class Demo {
+public class FoodsTableRedis {
 
 	static String sql = null;
 	static DBHelper db1 = null;
 	static ResultSet ret = null;
 
 	public static void main(String[] args) {
+		RedisClient FoodsTable_Redis = new RedisClient();
 		sql = "select *from food";//SQL语句
 		db1 = new DBHelper(sql);//创建DBHelper对象
-
 		try {
 			ret = db1.pst.executeQuery();//执行语句，得到结果集
 			while (ret.next()) {
-				String foodid = ret.getString(1);
-				String food_stock = ret.getString(2);
-				String food_price = ret.getString(3);
-				System.out.println(foodid + "\t" + food_stock + "\t" + food_price  );
+				FoodsTable_Redis.shardedJedis.hincrBy("FoodsTable",ret.getString(1),Integer.parseInt(ret.getString(2)));
 			}//显示数据
 			ret.close();
 			db1.close();//关闭连接
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		
 	}
 
 }
